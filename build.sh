b08381d6be3ee6926287415070e77963fa5323f5
Rafael <rafaeldiasaragao@gmail.com>
2024-12-27 14:06:10 -0300

    new file:   build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+apt-get update && apt-get install -y \
+    build-essential \
+    cmake \
+    libopenblas-dev \
+    liblapack-dev \
+    libx11-dev \
+    libatlas-base-dev
+
+pip install -r requirements.txt
